3e1c5df52a7312a810edfe92f85d0519d7c71843
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-07 21:04:21 +0600

    update code

--- solutions.ts
+++ solutions.ts
@@ -22,3 +22,78 @@ function checkType(value: StringOrNumber): string {
 
 
 // solution of problem4
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+const person = {
+  id: 1,
+  name: "John Doe",
+  age: 21
+};
+
+// solution of problem5
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+ isRead?: boolean;
+}
+
+function toggleReadStatus(book: Book) {
+  return {
+    ...book,
+    isRead: true
+  };
+}
+
+const myBook: Book = {
+  title: "TypeScript Guide",
+  author: "Jane Doe",
+  publishedYear: 2024
+};
+
+
+
+// solution of problem6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+
+
+
+// solution of problem7
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  const set2 = new Set(arr2);
+  const result: number[] = [];
+
+  for (const num of arr1) {
+    if (set2.has(num)) {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
