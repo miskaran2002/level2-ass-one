3680848c73b59462145bb90fdd277d5865e30c51
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2026-05-07 18:32:05 +0600

    update code

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,8 @@
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((value) => value % 2 === 0);
 }
+
+// solution of problem2
+function reverseString(text: string): string {
+  return text.split("").reverse().join("");
+}
